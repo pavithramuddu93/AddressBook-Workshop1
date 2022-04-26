cd5e0a8b723de64d09459cd08400b0077cd271af
pavithramuddu93 <pavithramuddu93>
2022-04-26 11:18:41 +0530

    UC21 added

--- src/main/java/blz1/MultipleAddressBook.java
+++ src/main/java/blz1/MultipleAddressBook.java
@@ -8,46 +8,49 @@ import java.util.List;
 
 interface MultipleAddressBook {
 
-    public void addAddressBook(String bookName, String firstName, String lastName, String address, String city, int zip,
-                               String state, long phoneNumber, String email);
-
-    public void getContact();
+    void addAddressBook(String bookName, String firstName, String lastName, String address, String city, int zip,
+                        String state, long phoneNumber, String email);
+    void getContact();
 
     boolean equals(String firstName);
 
-    public void editContact();
+    void editContact();
 
-    public void deleteEntry();
+    void deleteEntry();
 
-    public boolean makeChoice() throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException;
+    boolean makeChoice() throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException;
 
-    public void viewPersonByCity();
+    void viewPersonByCity();
 
-    public void viewPersonByState();
+    void viewPersonByState();
 
-    public void getContactByCityOrState();
+    void getContactByCityOrState();
 
-    public void sortAlphabetically();
+    void sortAlphabetically();
 
-    public void sortCityStateOrZip();
+    void sortCityStateOrZip();
 
-    public void writeAddressBookInFiles(AddressBookImplement.IOService ioService);
+    void writeAddressBookInFiles(AddressBookImplement.IOService ioService);
 
-    public void readAddressBookInFiles(AddressBookImplement.IOService ioService);
+    void readAddressBookInFiles(AddressBookImplement.IOService ioService);
 
-    public void writeAddressBookInCSV() throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException;
+    void writeAddressBookInCSV() throws CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException;
 
-    public void readAddressBookInCSV() throws IOException;
+    void readAddressBookInCSV() throws IOException;
 
-    public List<AddressBook> readAddressBookData();
+    List<AddressBook> readAddressBookData();
 
-    public boolean checkAddressBookSyncWithDB(String name);
+    boolean checkAddressBookSyncWithDB(String name);
 
-    public void updateAddressBook(String name, String address);
+    void updateAddressBook(String name, String address);
 
     AddressBook getAddressBookData(String name);
 
-    public List<AddressBook> readFilteredAddressBookData(String stateName);
+    List<AddressBook> readFilteredAddressBookData(String stateName);
 
-    public void addContacts(String bookName, String firstName, String lastName, String add, String city, String state, int zip, long phone, String email);
+    void addContacts(String bookName, String firstName, String lastName, String add, String city, String state, int zip, long phone, String email);
+
+    long countEntries();
+
+    void addContactDBWithThreads(List<AddressBook> addressBooks);
 }
